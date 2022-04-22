d91226c3f965cf3092adee0773c4bb32ba1d1db3
Anderson Pinto <anderson.p20@gmail.com>
2022-04-21 21:00:48 -0300

    DTO

--- backend/src/main/java/com/andersonpinto/dscatalog3/services/CategoryService.java
+++ backend/src/main/java/com/andersonpinto/dscatalog3/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.andersonpinto.dscatalog3.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.andersonpinto.dscatalog3.dto.CategoryDTO;
 import com.andersonpinto.dscatalog3.entities.Category;
 import com.andersonpinto.dscatalog3.repositories.CategoryRepository;
 
@@ -16,8 +18,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list =  repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
